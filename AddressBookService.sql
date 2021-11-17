fa9526e73c3ac818d66b3d8b2caa9acfc0b8d1bc
Afroj Satwilkar <afrozsatvilkar2014@gmail.com>
2021-11-17 13:30:21 +0530

    Create a Address Book Table

--- AddressBookService.sql
+++ AddressBookService.sql
@@ -3,3 +3,6 @@ create database AddressBookService;
 show databases;
 use AddressBookService;
 
+-- UC2
+create table aadress_book(first_name varchar(250), last_name varchar(250), address varchar(250), city varchar(250), state varchar(250), zip char(10), phone_number char(10), email varchar(120));
+desc aadress_book;
