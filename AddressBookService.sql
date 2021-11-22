482c1f98bbbbab1e8a01c32c01bad47cb508a966
Afroj Satwilkar <afrozsatvilkar2014@gmail.com>
2021-11-22 12:21:23 +0530

    Size of address book table by city and state

--- AddressBookService.sql
+++ AddressBookService.sql
@@ -24,3 +24,7 @@ select * from aadress_book;
 
 -- UC6
 select * from aadress_book where city='Mumbai' or state='Maharashtra';
+
+-- UC7
+select count(city) from aadress_book;
+select count(state) from aadress_book;
